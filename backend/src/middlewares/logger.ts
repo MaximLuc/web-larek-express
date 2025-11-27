370c688cf204b01ab991e46f64c37c15a0e6dbfc
Max <maximhop200307@gmail.com>
2025-11-27 20:32:57 +0200

    feat: обновить логи и структуру проекта, добавив пути для логов и файл .env.example

--- backend/src/middlewares/logger.ts
+++ backend/src/middlewares/logger.ts
@@ -1,16 +1,20 @@
 import winston from 'winston';
 import expressWinston from 'express-winston';
+import path from 'path';
+
+const logsPathRequests = path.join(__dirname, '../public/temp/requests.log');
+const logsPathErrors = path.join(__dirname, '../public/temp/errors.log');
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'logs/requests.log' }),
+    new winston.transports.File({ filename: logsPathRequests }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'logs/errors.log' }),
+    new winston.transports.File({ filename: logsPathErrors }),
   ],
   format: winston.format.json(),
 });
